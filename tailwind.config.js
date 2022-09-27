3e61ae23273e8ad88acd889cf5eb60c5cefae1b3
ErickyAll <erick227.ea21@gmail.com>
2022-09-26 23:05:53 -0300

    Add Web project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/Background.png')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 57.90%, #E1D55D 23.57%)',
+        'game-gd':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    }
+  },
+  plugins: []
+}
